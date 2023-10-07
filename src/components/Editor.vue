87e8dda29a42f1b42c5a2afcfdd8ba76302604dd
Katoshi12 <bulat.bulatov.99@bk.ru>
2023-10-08 01:43:25 +0300

    Add functions(undo,redo,textTransform,img,copy html)

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,4 +1,85 @@
 <script setup lang="ts">
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+
+const editor = ref<HTMLDivElement | null>(null);
+let historyIndex = -1;
+const editorHistory: string[] = [];
+
+const updateHistory = () => {
+  if (editor.value) {
+    editorHistory[historyIndex] = editor.value.innerHTML
+    editorHistory.splice(historyIndex)
+    editorHistory.push(editor.value.innerHTML)
+  }
+};
+
+const undo = () => {
+  //TODO REFACTORING
+  if (historyIndex > 0) {
+    historyIndex--;
+    if (editor.value) {
+      editor.value.innerHTML = editorHistory[historyIndex];
+    }
+  }
+};
+
+const redo = () => {
+  //TODO REFACTORING
+  if (historyIndex < editorHistory.length) {
+    console.log(editorHistory.length)
+    historyIndex++;
+    if (editor.value) {
+      editor.value.innerHTML = editorHistory[historyIndex];
+    }
+  }
+};
+
+const textTransform = (command: string, value: string | null = null) => {
+  if (editor.value) {
+    editor.value.focus();
+    document.execCommand(command, false, value);
+    updateHistory();
+  }
+};
+
+const transformImg = () => {
+  //TODO
+  const url = prompt('Введите ссылку на картинку');
+  if (url && editor.value) {
+    const imgTag = `<img src="${url}" alt="Добавленное изображение"/>`;
+    editor.value.innerHTML += imgTag;
+    updateHistory();
+  }
+};
+
+const copyHtml = async () => {
+  if (editor.value) {
+    await navigator.clipboard.writeText(editor.value.innerHTML).then(()=> {
+      alert('Текст успешно скопирован!');
+    })
+  }
+};
+
+const handleFocus = () => {
+  if (editor.value && editor.value.innerHTML === 'Введите текст') {
+    editor.value.innerHTML = '';
+  }
+};
+
+onMounted(() => {
+  if (editor.value) {
+    editor.value.innerHTML = 'Введите текст';
+    updateHistory();
+    editor.value.addEventListener('focus', handleFocus);
+  }
+});
+
+onBeforeUnmount(() => {
+  if (editor.value) {
+    editor.value.removeEventListener('focus', handleFocus);
+  }
+  editorHistory.length = 0;
+});
 </script>
 
 <template>
@@ -12,32 +93,22 @@
           <button @click="redo">
             <img src="@/assets/img/redo.svg" alt="Кнопка вперед">
           </button>
-          <button @click="transformTitle">
+          <button @click="textTransform('formatBlock', '<h1>')">
             <img src="@/assets/img/headline.svg" alt="Кнопка заголовок">
           </button>
-          <button @click="transformParagraph">
+          <button @click="textTransform('formatBlock', '<p>')">
             <img src="@/assets/img/paragraph.svg" alt="Кнопка параграф">
           </button>
           <button @click="transformImg">
             <img src="@/assets/img/img-prompt.svg" alt="Кнопка добавить фото">
           </button>
-          <p @click="">Скопировать HTML</p>
+          <p @click="copyHtml">Скопировать HTML</p>
         </div>
       </div>
     </section>
     <main class="main-block">
       <section>
-        <div ref="editor" class="editor" id="editor" contenteditable="true">
-          Таким образом консультация с широким активом представляет
-          собой интересный эксперимент проверки позиций, занимаемых участниками в отношении поставленных задач. С другой
-          стороны постоянное информационно-пропагандистское обеспечение нашей деятельности представляет собой интересный
-          эксперимент проверки форм развития. Идейные соображения высшего порядка, а также укрепление и развитие
-          структуры влечет за собой процесс внедрения и модернизации соответствующий условий активизации. Задача
-          организации, в особенности же реализация намеченных плановых заданий играет важную роль в формировании
-          дальнейших направлений развития. Повседневная практика показывает, что постоянное
-          информационно-пропагандистское обеспечение нашей деятельности играет важную роль в формировании существенных
-          финансовых и административных условий.
-        </div>
+        <div ref="editor" class="editor" id="editor" contenteditable="true" @focus="handleFocus"></div>
       </section>
     </main>
   </div>
